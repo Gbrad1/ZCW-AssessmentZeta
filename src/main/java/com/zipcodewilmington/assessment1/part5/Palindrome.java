9fe49acc2202c019bff04ea6036ce3d75409ef33
Giles Bradford <gbradford@TommyRogers.localdomain>
2020-02-18 11:36:34 -0500

    saving

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -2,7 +2,18 @@ package com.zipcodewilmington.assessment1.part5;
 
 public class Palindrome {
 
-    public Integer countPalindromes(String input){
-        return null;
+    public Integer countPalindromes(String input) {
+        Integer count = 0;
+        String toCheck = "";
+        for (int i = 0; i <= input.length(); i++) {
+            for (int s = i + 1; s <= input.length(); s++) {
+                toCheck = input.substring(i, s);
+                String reversed = new StringBuilder(toCheck).reverse().toString();
+                if (toCheck.equals(reversed)) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
